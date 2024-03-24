e85abb19a74859a668da082f850117e3683ff45b
amirhon1 <163624411+amirhon1@users.noreply.github.com>
2024-03-24 12:43:22 +0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+what = input("что делаем")
+
+a = float ( input("введите первое число: ") )
+b = float ( input("введите второе число: ") )
+if what == "+":
+    c = a + b
+    print("результат: " + str(c))
+elif what == "-":
+    c = a - b
+    print("результат: " + str(c))
+elif what == "/":
+    c = a / b
+    print("результат: " + str(c))
+if what == "*":
+    c = a * b
+    print("результат: " + str(c))
+else:
+    print ("выбрана неверная операция")
